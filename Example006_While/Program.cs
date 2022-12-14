a1e4e0239a0d399a6f941059001c5f236bcf8d1c
Egor Sazhnev <hard-link@mail.ru>
2022-12-14 14:22:40 +1000

    project nowhile

---
+++ Example006_While/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+int c = new Random().Next(1,100);
+int d = new Random().Next(1,100);
+int e = new Random().Next(1,100);
+int max = a;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Максимальное = ");
+Console.WriteLine(max);
