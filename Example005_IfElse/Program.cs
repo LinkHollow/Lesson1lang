d53a33419c34c4d76bf7c85a6d28d67a29112aab
Egor Sazhnev <hard-link@mail.ru>
2022-12-14 14:06:32 +1000

    project igelse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи Имя Пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "егор")
+{
+    Console.WriteLine("Ура, это Егор!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
